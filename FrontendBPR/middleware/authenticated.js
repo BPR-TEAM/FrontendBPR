36abb0f019326635ab4c339a9f081ff71362d7cb
iandrei250 <prtdace@gmail.com>
2022-01-22 11:22:59 +0100

    middleware change

--- FrontendBPR/middleware/authenticated.js
+++ FrontendBPR/middleware/authenticated.js
@@ -1,10 +1,10 @@
 import { getCookie } from "../static/cookie.js";
 
-export function authenticated() {
+export function authenticated({ redirect }) {
   let auth = getCookie("auth");
   console.log(auth);
   // If the user is not authenticated
   if (!auth || auth === "" || auth === "undefined") {
-    return this.$router.push("/");
+    return redirect("/");
   }
 }
